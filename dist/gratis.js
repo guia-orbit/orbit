f476b60a4f3978240cb2ec05191f17d07372cfff
Michael Wells <info@sygnal.com>
2023-07-29 07:54:25 +0000

    Gratis button

---
+++ dist/gratis.js
@@ -0,0 +1,10 @@
+(() => {
+  // src/gratis.ts
+  var Gratis = class {
+    constructor() {
+    }
+    init() {
+    }
+  };
+})();
+//# sourceMappingURL=gratis.js.map
---
+++ src/gratis.ts
@@ -0,0 +1,44 @@
+
+/** 
+ * ORBIT 
+ * Gratis  
+ * 
+ * Free events
+ * 
+ */ 
+
+
+
+export class Gratis {
+
+    constructor() {
+    }
+
+    // Installs hoje (set date filter to today)
+    // sets the date using flatpickr. 
+    init() {
+
+        // document.addEventListener('DOMContentLoaded', function() {
+        //   var button = document.getElementById('gratis-button');
+        //   button.addEventListener('click', filterAndScroll);
+        // });
+        
+        // function filterAndScroll() {
+        //   var checkbox = document.getElementById('gratis-checkbox');
+        //   checkbox.click();
+        
+        //   var container = document.getElementById('content-filter');
+        //   container.scrollIntoView({ behavior: 'smooth' });
+        // }
+
+    }
+
+}
+
+    
+    
+
+    
+    
+    
+    
